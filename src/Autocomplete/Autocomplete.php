90912ea49a720437b885586e4afdfeea9dc1b62c
RehanyTamas <Rt19991231>
2023-11-06 13:25:50 +0100

    add getmatch functionality

--- src/Autocomplete/Autocomplete.php
+++ src/Autocomplete/Autocomplete.php
@@ -6,8 +6,29 @@ use DataStructure\Trie;
 
 class Autocomplete extends Trie
 {
-    public function getMatches(string $prefix)
+    public function getMatches(string $prefix): array
     {
-        // Your code goes here
+        $node = $this->root;
+        for ($i=0; $i < strlen($prefix);$i++){
+            $char = $prefix[$i];
+            if (!isset($node->children[$char])) {
+                return[];
+            }
+            $node = $node->children[$char];
+        }
+
+        $matches = [];
+        $this->findWordsWithPrefix($node, $prefix, $matches);
+        return $matches;
+
+    }
+    private function findWordsWithPrefix($node, $currentPrefix, &$matches) {
+        if ($node->endOfWord) {
+            $matches[] = $currentPrefix;
+        }
+
+        foreach ($node->children as $char => $childNode) {
+            $this->findWordsWithPrefix($childNode, $currentPrefix . $char, $matches);
+        }
     }
 }
